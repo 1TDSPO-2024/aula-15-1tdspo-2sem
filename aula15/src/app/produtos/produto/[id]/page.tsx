79d936ba7038f878d228962d3e3e7426b7131bfa
SunaUezuri <wesley.jane.santos@gmail.com>
2024-09-26 22:16:00 -0300

    Realizando a passagem de parâmetros.

--- aula15/src/app/produtos/produto/[id]/page.tsx
+++ aula15/src/app/produtos/produto/[id]/page.tsx
@@ -1,12 +1,36 @@
+"use client"
+
+import { TipoProduto } from "@/types"
+import { useState, useEffect } from "react"
+
 
 export default function Produto({params}:{params:{id:number}}) {
 
-    const [produto, setProduto]
+    const [produto, setProduto] = useState<TipoProduto>([])
+
+    useEffect(() => {
+      
+      const chamadaApi = async () => {
+        const response = await fetch(`/api/base-produtos/${params.id}`)
+        const dados = await response.json()
+        setProduto(dados)
+      }
+
+      chamadaApi();
+    }, [params.id])
 
   return (
     <div>
         <h1>Produto</h1>
         <p>ID : {params.id}</p>
+        <img src={produto.imagem} alt="Imagem Produto" />
+        <p>Nome: {produto.nome}</p>
+        <p>Preço: {produto.preco}</p>
+        <p>Marca</p>
+        <p>Cor</p>
+        <p></p>
+        <p></p>
+        <p></p>
     </div>
   )
 }
