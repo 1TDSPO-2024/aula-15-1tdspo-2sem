d7c0441fb4d45853dc5df03fa820c417b342e2c3
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-26 22:25:50 -0300

    adicionando get by id

--- aula15/src/app/produtos/produto/[id]/page.tsx
+++ aula15/src/app/produtos/produto/[id]/page.tsx
@@ -1,24 +1,27 @@
+"use client";
+import { TipoProduto } from "@/types";
+import { useEffect, useState } from "react";
 
-export default function Produto({params}:{params:{id:number}}) {
+export default function Produto({ params }: { params: { id: number } }) {
+  const [produto, setProduto] = useState<TipoProduto>();
 
-    
+  useEffect(() => {
+    const chamadaDaApi = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/base-produtos/${params.id}`
+      );
+      const dados = await response.json();
+      setProduto(dados);
+    };
+
+    chamadaDaApi();
+  }, []);
 
   return (
     <div>
-        <h1>Produto</h1>
-        <p>ID : {params.id}</p>
+      <h1>Produto</h1>
+      <p>ID: {produto?.id}</p>
+      <p>Nome: {produto?.nome}</p>
     </div>
-  )
+  );
 }
-
-
-// export default function Produto({params}:{params:{id:number}}) {
-//     return (
-//       <div>
-//           <h2>Produto</h2>
-//           <div>
-//               <h3>ID: {params.id}</h3>
-//           </div>
-//       </div>
-//     )
-//   }
