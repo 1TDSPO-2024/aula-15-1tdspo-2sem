ecad30cecefd2c8371b716975d6da5fde786aa54
JulioDev01 <juliocesarmaciel21@gmail.com>
2024-09-26 22:51:43 -0300

    Manipulando API

--- aula15/src/app/produtos/page.tsx
+++ aula15/src/app/produtos/page.tsx
@@ -1,65 +1,25 @@
+"use client";
+import { TipoProduto } from "@/types";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
   //Criar uma lista de objetos de produtos eletrônicos co os seguintes dados:
   //id,nome, preço, marca, cor, modelo, quantidade, categoria, imagem
-  const produtos = [
-    {
-      id: 1,
-      nome: "Smartphone",
-      preco: 1500.0,
-      marca: "Apple",
-      cor: "Preto",
-      modelo: "iPhone 13",
-      quantidade: 10,
-      categoria: "Eletrônicos",
-      imagem: "https://picsum.photos/200/300",
-    },
-    {
-      id: 2,
-      nome: "Notebook",
-      preco: 2500.0,
-      marca: "Dell",
-      cor: "Branco",
-      modelo: "Inspiron 15",
-      quantidade: 5,
-      categoria: "Eletrônicos",
-      imagem: "https://picsum.photos/200/300",
-    },
-    {
-      id: 3,
-      nome: "Tablet",
-      preco: 800.0,
-      marca: "Samsung",
-      cor: "Preto",
-      modelo: "Galaxy Tab S8",
-      quantidade: 15,
-      categoria: "Eletrônicos",
-      imagem: "https://picsum.photos/200/300",
-    },
-    {
-      id: 4,
-      nome: "Impressora",
-      preco: 300.0,
-      marca: "HP",
-      cor: "Branco",
-      modelo: "OfficeJet Pro",
-      quantidade: 20,
-      categoria: "Eletrônicos",
-      imagem: "https://picsum.photos/200/300",
-    },
-    {
-      id: 5,
-      nome: "Fone de Ouvido",
-      preco: 200.0,
-      marca: "Sony",
-      cor: "Preto",
-      modelo: "WH-1000XM4",
-      quantidade: 30,
-      categoria: "Eletrônicos",
-      imagem: "https://picsum.photos/200/300",
-    },
-  ];
+  
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+  
+  useEffect(() => {
+      const chamadaApi = async ()=>{
+        const response = await fetch("http://localhost:3001/api/base-produtos");
+        const dados = await response.json();
+        setProdutos(dados);
+      };
+
+      chamadaApi();
+
+  }, [])
+
 
   //Iterar a lista de produtos em uma tabela, utilizando, head,body e footer.
 
--- aula15/src/app/produtos/produto/[id]/page.tsx
+++ aula15/src/app/produtos/produto/[id]/page.tsx
@@ -1,24 +1,44 @@
+"use client";
+
+import { TipoProduto } from "@/types";
+import Image from 'next/image';
+import { useEffect, useState } from "react";
 
 export default function Produto({params}:{params:{id:number}}) {
 
+  const [produto, setProduto] = useState<TipoProduto>({
+    id:0,
+    nome:"",
+    preco:0,
+    marca:"",
+    cor:"",
+    modelo:"",
+    quantidade:0,
+    categoria:"",
+    imagem:""
+  });
+  
+  useEffect(() =>{
+    const chamadaApi = async ()=>{
+      const response = await fetch(`http://localhost:3001/api/base-produtos/${params.id}`);
+      const dados = await response.json();
+      setProduto(dados);
+    }
+    chamadaApi();
+  }, [])
     
-
   return (
     <div>
         <h1>Produto</h1>
         <p>ID : {params.id}</p>
+        <p>Nome : {produto?.nome}</p>
+        <p>Preço : {produto?.preco}</p>
+        <p>Marca : {produto?.marca}</p>
+        <p>Cor : {produto?.cor}</p>
+        <p>Quantidade : {produto?.quantidade}</p>
+        <p>Modelo : {produto?.modelo}</p>
+        <p>Categoria : {produto?.categoria}</p>
+
     </div>
   )
 }
-
-
-// export default function Produto({params}:{params:{id:number}}) {
-//     return (
-//       <div>
-//           <h2>Produto</h2>
-//           <div>
-//               <h3>ID: {params.id}</h3>
-//           </div>
-//       </div>
-//     )
-//   }
