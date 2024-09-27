6e9e0a195da1e75bc5aa7c24d042c02d21403376
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-26 22:46:08 -0300

    adicionando imagem

--- aula15/src/app/produtos/produto/[id]/page.tsx
+++ aula15/src/app/produtos/produto/[id]/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { TipoProduto } from "@/types";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Produto({ params }: { params: { id: number } }) {
-  const [produto, setProduto] = useState<TipoProduto>();
+  const [produto, setProduto] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0,
+    marca: "",
+    modelo: "",
+    descricao: "",
+    imagem: "",
+  });
 
   useEffect(() => {
     const chamadaDaApi = async () => {
@@ -26,7 +35,9 @@ export default function Produto({ params }: { params: { id: number } }) {
       <p>Marca: {produto?.marca}</p>
       <p>Modelo: {produto?.modelo}</p>
       <p>Descrição: {produto?.descricao}</p>
-      <img src={produto?.imagem} alt={produto?.nome} height="100" width="100" />
+      <figure>
+        <Image src={produto?.imagem} alt={produto?.nome} width={100} height={100}/>
+      </figure>
     </div>
   );
 }
